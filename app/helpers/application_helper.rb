0c604d97c1671cfbd34c69364127f9f5e91c43e7
Sam Pierson <sam@sampierson.com>
2011-01-24 22:12:10 -0800

    Add some minimal style to shape the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Include ancestors, e.g. Admin::UsersController -> "admin_users"
+  def underscored_controller_name
+    controller.class.name.underscore.gsub('_controller', '').gsub('/', '_')
+  end
+
+  def body_css_classes
+    "#{underscored_controller_name} #{underscored_controller_name}_#{action_name}"
+  end
+
 end
