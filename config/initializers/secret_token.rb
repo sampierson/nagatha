e82f5827f8665d1fa44c46ce6563942978cfa56a
Sam Pierson <sam@sampierson.com>
2011-01-24 22:12:04 -0800

    Virgin Rails 3.0.3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nagatha::Application.config.secret_token = '5c3123b40d57cd6fd141415cc4209038bfaa2077560063c13d41bfd34a4a6a2395034a589fd635b9178fd8eb792337f186ea132e7a4505b5729488d122fed93d'
