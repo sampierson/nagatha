90a37214e6af38b5b989d8855853ace8e90c8d8d
Sam Pierson <sam@sampierson.com>
2011-01-24 22:12:22 -0800

    [CanCan] Add User#admin?

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  def admin?
+    role == 'admin' && confirmed_at && !locked_at
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,15 @@ require 'spec_helper'
 describe User do
   fixtures :users
   
+  describe "#admin" do
+    it "should return true if the user is a confirmed admin user, false otherwise" do
+      users(:confirmed_admin_user).should be_admin
+      users(:unconfirmed_admin_user).should_not be_admin
+      users(:locked_admin_user).should_not be_admin
+      users(:unconfirmed_user).should_not be_admin
+      users(:unconfirmed_user).should_not be_admin
+      users(:locked_user).should_not be_admin
+    end
+  end
+
 end
